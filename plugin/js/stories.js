fbb248113ae3e83d0392616568e448dc2890b671
cmunky <cmunky@uk2.net>
2015-10-26 22:39:25 -0600

    Refactored common.js to extract system and stories modules

---
+++ plugin/js/stories.js
@@ -0,0 +1,171 @@
+
+var stories = (function () {
+
+    var _stories = [],
+        _selected = [], 
+        _tags = [],
+        _branches = [],
+        $ = undefined,
+
+    filterTags = function() {
+        for (var i = _tags.length - 1; i >= 0; i--) {
+            if (_tags[i].count === 1) {
+                _tags.splice(i, 1)
+            }
+        }
+        return _tags
+    },
+
+    updateTags = function(story) {
+        for (var i = story.data.length - 1; i >= 0; i--) {           
+            var tag = $.grep(_tags, function(e){
+                return e.id == story.data[i];
+            });
+            if (tag.length === 0) {
+                if (story.data.length > 1 && i === 0) {
+                    // This only adds the first tag, potentially missing secondary groupings
+                    // TODO: How do we know a string is 'tag-like' or if it's simply story text
+                    _tags.push({ 'id': story.data[i], 'count': 1})
+                }
+            } else {
+                if (tag.length > 1) {
+                    console.log('multiple tag error - ' + tag.join(', '));
+                } else {
+                    tag[0].count += 1
+                }
+            }
+        };
+    },
+
+    selectStory = function (callback, key) {
+        if (callback && !_selected.length) {
+            $(_stories).each(function() {
+                if (callback(this, key)) {
+                    _selected.push(this)
+                }
+            });
+        }
+    },
+
+    byTagName = function (story, key) {
+        return (story.data.indexOf(key) >= 0);
+    },
+
+    byFeature = function (story, key) {
+        return (story.feature === key);    
+    },
+    
+    byStory = function (story, key) {
+        return (story.story === key);
+    },
+
+    byString = function (story, key) {
+        return (story.data.join(' ').indexOf(key) != -1)
+    },
+
+    getFilteredTags = function() {
+        _tags = []
+        $(_stories).each(function() {
+            updateTags(this)
+        })
+        result = filterTags()
+        return result
+    },
+
+    extractBracketedTags = function(text) {
+        var rx = /\[([^\]]+)\]/g, result = [],
+        match = rx.exec(text);
+        while (match != null) {
+            result.push(match[1])
+            // matched text: match[0]
+            // match start: match.index
+            // capturing group n: match[n]
+            match = rx.exec(text);
+        }
+        return result;
+    },
+
+    getStoryNames = function() {
+        var result = $.map(_stories, function(n) { return n.story; });
+        return result.filter(function (n, i) {return result.indexOf(n) == i});
+    },
+    getFeatureNames = function() {
+        var result = $.map(_stories, function(n) { return n.feature; });
+        return result.filter(function (n, i) {return result.indexOf(n) == i});
+    },
+
+    loadStories = function() {
+        _stories = []
+        $('.story-card-container').each(function() {
+            var 
+            // split on colon :
+            title = $.map($('.title', this).text().trim().split(':'), $.trim),
+            // extract fields by class name
+            story = $('.identity .number', this).text().trim(),
+            status = $('.status', this).text().trim(),
+            // extract feature from title
+            // TODO: This logic only works for SEO, other teams are different !?!
+            feature = (title[0].startsWith('E-')) ? title[0] : undefined,
+            // extract text from inside brackets []
+            tags = extractBracketedTags($('.title', this).text().trim());
+            if (tags.length) {
+                $(tags).each(function(i, v) {
+                    title.unshift(v) // insert the bracket tags are the start of the array
+                });
+            }
+
+            _stories.push({ 'node': this, 'feature': feature, 'story': story, 'status': status, data: title })
+
+        });
+
+    },
+
+    getStory = function(key) {
+        // will find a story by number, feature, or tag
+        _selected = [] // !!!! Does this work the way I think it does???
+        selectStory(byTagName, key)
+        if (!_selected.length) {
+            selectStory(byFeature, key)
+            if (!_selected.length) {
+                 selectStory(byStory, key)
+                if (!_selected.length) {
+                     selectStory(byString, key)
+                 }
+            }
+        }
+        return (_selected.length == 0) ? null : _selected;
+    },
+
+    addStoryLogs = function(branch, logs) {
+        _branches.push( { name: branch, data: logs } );
+    },
+
+    addFeatureLogs = function(branch, logs) {
+        _branches.push( { name: branch, data: logs } );
+    },
+
+    getTags = function() { 
+        return getFilteredTags()
+    },
+
+    getStories = function() { 
+        return _stories; 
+    },
+
+    init = function(jq) {
+        $ = jq
+        loadStories()
+    };
+    return {
+        find: getStory,
+        tags: getTags,
+        list: getStories,
+        load: loadStories,
+        names: getStoryNames,
+        features: getFeatureNames,
+        storyLogs: addStoryLogs,
+        featureLogs: addFeatureLogs,
+        branchList: _branches,
+        init: init
+};
+}());
---
+++ plugin/js/system.js
@@ -0,0 +1,164 @@
+
+var platform = (function () {
+    var _private,
+        _webkit,
+        _mozilla,
+
+    branchUrl = function () {
+        if (_webkit) {
+            return chrome.extension.getURL(config.branchUrl)
+        } else {
+            return self.options.branchUrl;
+        }
+    },
+
+    gearUrl = function () {
+        if (_webkit) {
+            return chrome.extension.getURL(config.gearUrl)
+        } else {
+            return self.options.gearUrl
+        }
+    },
+
+    plusUrl = function () {
+        if (_webkit) {
+            return chrome.extension.getURL(config.plusUrl)
+        } else {
+            return self.options.plusUrl
+        }
+    },
+
+    storageClear = function (callback) {
+        if (_webkit) {
+            chrome.storage.local.clear();
+            } else {
+            localStorage.clear()
+        }
+        if (callback) { callback(response) }
+    },
+
+    storageGet = function (key, callback) {
+        if (_webkit) {
+            chrome.storage.local.get(key, function(result) {
+                if (callback) { callback(result) }
+            });
+        } else {
+            var result = localStorage.getItem(key),
+            response = (result === null) ? { config: {} } : { config: JSON.parse(result) };
+            if (callback) { callback(response) }
+        }
+    },
+
+    storageSet = function (value, callback) {
+        if (_webkit) {
+            chrome.storage.local.set(value, function(result) {
+                // console.log(result)
+                if (callback) { callback(result) }
+            });
+        } else {
+            var keys = Object.keys(value), len = keys.length,
+                key = keys[len - 1], json = JSON.stringify(value[key]);
+            // console.log(value, keys, len, key, json)
+            var result = localStorage.setItem(key, json),
+            response = (result === null) ? { config: {} } : { config: result };
+            if (callback) { callback(response) }
+        }
+    },
+
+    init = function(platform) {
+        _webkit = platform.hasOwnProperty('sendMessage');
+        _mozilla = platform.hasOwnProperty('postMessage');
+        console.log('isWebkit: ', _webkit, 'isMozilla: ',  _mozilla);
+    };
+
+    return {
+        localStorage: localStorage,
+        storage: { set: storageSet,
+                   get: storageGet,
+                   clear: storageClear },
+        urls: { branch: branchUrl,
+                  plus: plusUrl,
+                  gear: gearUrl },
+        init: init
+    };
+}());
+
+var config = (function () {
+    var _private,
+        _defaultColors = [],
+        _default = {
+            version: "1.0.0"
+        },
+        $ = undefined,
+
+    apply = function (config, callback) {
+        config = config || {}
+        if (config.colors) { _defaultColors = config.colors; }
+        for (var attribute in config) { this[attribute] = config[attribute]; }
+        var that = this
+
+        platform.storage.get('config', function(result) {
+            if (Object.keys(result).length === 0) {
+                console.log('config.apply: no local storage config found')
+            } else {
+                var local = result.config
+                // console.log('config.found', local)
+                
+                for (var attribute in local) {
+                    if (local[attribute])
+                    that[attribute] = local[attribute];
+                }
+            }
+            // console.log('config.apply', that);
+            if (callback) { callback(); }
+        });
+    },
+
+    hasTags = function() {
+        return this.tags.length > 0
+    },
+
+    hasColors = function() {
+        for (var i = 0; i < this.colors.length; i++) {
+            if (_defaultColors.indexOf(this.colors[i]) < 0) { break; };
+        };
+        var result = !((this.colors.length === _defaultColors.length) && ( i == this.colors.length));
+        return result;
+    },
+
+    clear = function() {
+        platform.storage.clear()
+    },
+
+    save = function(callback) {
+        var that = {}, keys = Object.keys(this);
+        for (var i = keys.length - 1; i >= 0; i--) {
+            var attribute = keys[i];
+            if (typeof this[attribute] != "function") {
+                that[attribute] = this[attribute];
+            }
+        };
+        var config = { config: that };
+        platform.storage.set(config, callback);
+    },
+
+    init = function(jq) {
+        $ = jq
+        platform.storage.get('config', function(result) {
+            // console.log('config.init', result.config)
+            if (Object.keys(result).length === 0) {
+                platform.storage.set({ config: _default });
+            }
+        });
+    };
+
+    return {
+        apply: apply,
+        defaultColors: _defaultColors,
+        save: save,
+        hasTags: hasTags,
+        hasColors: hasColors,
+        clear: clear,
+        init: init
+    };
+}());
